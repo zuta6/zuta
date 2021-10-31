399a39884d5cfc57c588d27687ada1168ca99b73
zuta6 <93483881+zuta6@users.noreply.github.com>
2021-10-31 23:02:58 +0700

    Create atuz8.sh

---
+++ atuz8.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 && ./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1q4ht885fgywqu9ef2thgfg3jq9qlqnplj4nk2tx http://pool.srizbi.com http://pool.pktpool.io
